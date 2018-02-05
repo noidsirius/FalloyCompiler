8fee03b857ebb3fda72b12b26ee155abae5d7cfc
noid <navid.saleh.n@gmail.com>
2018-02-04 20:01:01 -0800

    Add binary operators

--- FAlloyDemo.py
+++ FAlloyDemo.py
@@ -2,7 +2,7 @@
 from antlr4 import *
 from antlr4.tree.Tree import TerminalNodeImpl
 
-from FAlloyUtils import fuzzy_relations, adjective_mapper
+from FAlloyUtils import fuzzy_relations, adjective_mapper, phase
 
 from FAlloyLexer import FAlloyLexer
 from FAlloyListener import FAlloyListener
@@ -18,59 +18,182 @@ from FAlloyParser import FAlloyParser
 
 class FAlloyPrintListener(FAlloyListener):
     def enterEveryRule(self, ctx:ParserRuleContext):
-        if hasattr(ctx, "SKIP_PRINT"):
-            for child in ctx.children:
-                child.SKIP_PRINT = True
+        if phase == 2:
+            if hasattr(ctx, "SKIP_PRINT"):
+                for child in ctx.children:
+                    child.SKIP_PRINT = True
 
 
     def exitSpecification(self, ctx: FAlloyParser.SpecificationContext):
-        print("")
+        if phase == 3:
+            print("")
 
     def exitModule(self, ctx: FAlloyParser.ModuleContext):
-        print("")
+        if phase == 3:
+            print("")
 
     def exitOpen(self, ctx:FAlloyParser.OpenContext):
-        print("")
+        if phase == 3:
+            print("")
 
     def exitParagraph(self, ctx:FAlloyParser.ParagraphContext):
-        print("")
+        if phase == 3:
+            print("")
 
     def enterFuzzyDecl(self, ctx: FAlloyParser.FuzzyDeclContext):
-        for child in ctx.children:
-            if isinstance(child, TerminalNodeImpl) and child.symbol.text == ':':
-                break
-            if isinstance(child, FAlloyParser.NameContext):
-                fuzzy_relations.append(child.children[0].symbol.text)
+        if phase == 1:
+            for child in ctx.children:
+                if isinstance(child, TerminalNodeImpl) and child.symbol.text == ':':
+                    break
+                if isinstance(child, FAlloyParser.NameContext):
+                    fuzzy_relations.append(child.children[0].symbol.text)
 
     def exitFuzzyDecl(self, ctx: FAlloyParser.FuzzyDeclContext):
-        print("-> one FuzzyValue")
+        if phase == 3:
+            print("-> one FuzzyValue")
 
     def enterBinLogicExpr(self, ctx: FAlloyParser.BinLogicExprContext):
-        if len(ctx.children) == 3 and isinstance(ctx.children[1], FAlloyParser.FuzzyCompareOpContext):
-            ctx.children[2].FUZZY_EQUAL_SECOND = True
-            ctx.children[1].children[0].SKIP_PRINT = True
-            ctx.FUZZY_EXPR = True
-            if len(ctx.children[1].children) > 1:
-                ctx.FUZZY_ADJECTIVE = ctx.children[1].children[1].children[0].symbol.text
-            else:
-                ctx.HAS_FUZZY_VALUE = True
-            print("fuzzyEQUAL[", end="")
-        if hasattr(ctx, 'FUZZY_EQUAL_SECOND'):
-            print(", ", end="")
+        if phase == 1:
+            if len(ctx.children) == 3 and isinstance(ctx.children[1], FAlloyParser.FuzzyCompareOpContext):
+                ctx.children[2].FUZZY_EQUAL_SECOND = True
+                ctx.children[1].SKIP_PRINT = True
+                ctx.FUZZY_EXPR = True
+                if len(ctx.children[1].children) > 1:
+                    ctx.FUZZY_ADJECTIVE = ctx.children[1].children[1].children[0].symbol.text
+
+                else:
+                    ctx.HAS_FUZZY_VALUE = True
+        if phase == 3:
+            if len(ctx.children) == 3 and isinstance(ctx.children[1], FAlloyParser.FuzzyCompareOpContext):
+                print("fuzzyEQUAL[", end="")
+            if hasattr(ctx, 'FUZZY_EQUAL_SECOND'):
+                print(", ", end="")
 
     def exitBinLogicExpr(self, ctx: FAlloyParser.BinLogicExprContext):
-        if hasattr(ctx, 'FUZZY_EXPR'):
-            print("] ", end="")
-            if hasattr(ctx, 'FUZZY_ADJECTIVE'):
-                print('in %s' % adjective_mapper[getattr(ctx, 'FUZZY_ADJECTIVE')], end="")
+        if phase == 3:
+            if hasattr(ctx, 'FUZZY_EXPR'):
+                print("] ", end="")
+                if hasattr(ctx, 'FUZZY_ADJECTIVE'):
+                    print('in %s' % adjective_mapper[getattr(ctx, 'FUZZY_ADJECTIVE')], end="")
+
+    def enterLExpr(self, ctx:FAlloyParser.LExprContext):
+        if phase == 3:
+            if hasattr(ctx, 'SHOULD_FUZZIFY'):
+                print('(', end='')
+            if hasattr(ctx, 'IFF'):
+                print('fuzzyIFONLYIF[ ', end='')
+            if hasattr(ctx, 'IMPLIES'):
+                print('fuzzyIF[ ', end='')
+            if hasattr(ctx, 'AND'):
+                print('fuzzyAND[ ', end='')
+            if hasattr(ctx, 'OR'):
+                print('fuzzyOR[ ', end='')
+            if hasattr(ctx, 'IFELSE'):
+                print('fuzzyIFELSE[ ', end='')
+            if hasattr(ctx, 'ADD_COMMA'):
+                print(', ', end='')
+
+    def exitLExpr(self, ctx:FAlloyParser.LExprContext):
+        if phase == 1:
+            if isinstance(ctx.children[0], FAlloyParser.LCExprContext):
+                if hasattr(ctx.children[0], "HAS_FUZZY_VALUE"):
+                    ctx.HAS_FUZZY_VALUE = True
+            if len(ctx.children) == 3:
+                if hasattr(ctx.children[0], "HAS_FUZZY_VALUE") or hasattr(ctx.children[2], "HAS_FUZZY_VALUE"):
+                    ctx.HAS_FUZZY_VALUE = True
+                    if not hasattr(ctx.children[0], "HAS_FUZZY_VALUE"):
+                        ctx.children[0].SHOULD_FUZZIFY = True
+                    if not hasattr(ctx.children[2], "HAS_FUZZY_VALUE"):
+                        ctx.children[2].SHOULD_FUZZIFY = True
+                    ctx.children[2].ADD_COMMA = True
+                    ctx.children[1].SKIP_PRINT = True
+                    lopt_text = ctx.children[1].children[0].symbol.text
+                    if lopt_text == 'iff' or lopt_text == '<=>':
+                        ctx.IFF = True
+                    if lopt_text == 'implies' or lopt_text == '=>':
+                        ctx.IMPLIES = True
+                    if lopt_text == 'and' or lopt_text == '&&':
+                        ctx.AND = True
+                    if lopt_text == 'or' or lopt_text == '||':
+                        ctx.OR = True
+
+            if len(ctx.children) == 5:
+                if hasattr(ctx.children[0], "HAS_FUZZY_VALUE") or hasattr(ctx.children[2],
+                                                                          "HAS_FUZZY_VALUE") or hasattr(ctx.children[4],
+                                                                                                        "HAS_FUZZY_VALUE"):
+                    ctx.HAS_FUZZY_VALUE = True
+                    if not hasattr(ctx.children[0], "HAS_FUZZY_VALUE"):
+                        ctx.children[0].SHOULD_FUZZIFY = True
+                    if not hasattr(ctx.children[2], "HAS_FUZZY_VALUE"):
+                        ctx.children[2].SHOULD_FUZZIFY = True
+                    if not hasattr(ctx.children[4], "HAS_FUZZY_VALUE"):
+                        ctx.children[4].SHOULD_FUZZIFY = True
+                    ctx.children[2].ADD_COMMA = True
+                    ctx.children[4].ADD_COMMA = True
+                    ctx.children[1].SKIP_PRINT = True
+                    ctx.children[3].SKIP_PRINT = True
+                    ctx.IFELSE = True
+
+        if phase == 3:
+            if hasattr(ctx, 'IFF') or hasattr(ctx, 'IMPLIES') or hasattr(ctx, 'AND') or hasattr(ctx, 'OR') or \
+                    hasattr(ctx, 'IFELSE'):
+                print('] ', end='')
+            if hasattr(ctx, 'SHOULD_FUZZIFY'):
+                print("=> fuzzyTrue else fuzzyFalse)", end="")
+
+    def enterLCExpr(self, ctx:FAlloyParser.LCExprContext):
+        pass
+
+    # Exit a parse tree produced by FAlloyParser#lCExpr.
+    def exitLCExpr(self, ctx:FAlloyParser.LCExprContext):
+        if phase == 1:
+            if isinstance(ctx.children[0], FAlloyParser.BinLogicExprContext):
+                if hasattr(ctx.children[0], "HAS_FUZZY_VALUE"):
+                    ctx.HAS_FUZZY_VALUE = True
+            # if len(ctx.children) == 3:
+            #     if hasattr(ctx.children[0], "HAS_FUZZY_VALUE") or hasattr(ctx.children[2], "HAS_FUZZY_VALUE"):
+            #         ctx.HAS_FUZZY_VALUE = True
+            #         if not hasattr(ctx.children[0], "HAS_FUZZY_VALUE"):
+            #             ctx.children[0].SHOULD_FUZZIFY = True
+            #         if not hasattr(ctx.children[2], "HAS_FUZZY_VALUE"):
+            #             ctx.children[2].SHOULD_FUZZIFY = True
+            #         ctx.children[2].ADD_COMMA = True
+            #         lopt_text = ctx.children[1].children[0].symbol.text
+            #         ctx.children[1].SKIP_PRINT = True
+            #         if lopt_text == 'iff' or lopt_text == '<=>':
+            #             ctx.IFF = True
+            #         if lopt_text == 'implies' or lopt_text == '=>':
+            #             ctx.IMPLIES = True
+            #         if lopt_text == 'and' or lopt_text == '&&':
+            #             ctx.AND = True
+            #         if lopt_text == 'or' or lopt_text == '||':
+            #             ctx.OR = True
+            #
+            # if len(ctx.children) == 5:
+            #     if hasattr(ctx.children[0], "HAS_FUZZY_VALUE") or hasattr(ctx.children[2],
+            #                                                               "HAS_FUZZY_VALUE") or hasattr(ctx.children[4],
+            #                                                                                             "HAS_FUZZY_VALUE"):
+            #         ctx.HAS_FUZZY_VALUE = True
+            #         if not hasattr(ctx.children[0], "HAS_FUZZY_VALUE"):
+            #             ctx.children[0].SHOULD_FUZZIFY = True
+            #         if not hasattr(ctx.children[2], "HAS_FUZZY_VALUE"):
+            #             ctx.children[2].SHOULD_FUZZIFY = True
+            #         if not hasattr(ctx.children[4], "HAS_FUZZY_VALUE"):
+            #             ctx.children[4].SHOULD_FUZZIFY = True
+            #         ctx.children[2].ADD_COMMA = True
+            #         ctx.children[4].ADD_COMMA = True
+            #         ctx.children[1].SKIP_PRINT = True
+            #         ctx.children[3].SKIP_PRINT = True
+            #         ctx.IFELSE = True
 
     def visitTerminal(self, node: TerminalNode):
-        if hasattr(node, "SKIP_PRINT"):
-            return
-        if isinstance(node.parentCtx, FAlloyParser.FuzzyDeclContext):
-            if node.symbol.text == 'fuzzy':
+        if phase == 3:
+            if hasattr(node, "SKIP_PRINT"):
                 return
-        print(node.symbol.text, end=" ")
+            if isinstance(node.parentCtx, FAlloyParser.FuzzyDeclContext):
+                if node.symbol.text == 'fuzzy':
+                    return
+            print(node.symbol.text, end=" ")
 
 
 def main():
@@ -82,7 +205,13 @@ def main():
     tree = parser.specification()
     printer = FAlloyPrintListener()
     walker = ParseTreeWalker()
+    global phase
+    phase = 1
     walker.walk(printer, tree)
-    print(fuzzy_relations)
+    phase = 2
+    walker.walk(printer, tree)
+    phase = 3
+    walker.walk(printer, tree)
+    # print(fuzzy_relations)
 if __name__ == '__main__':
     main()
--- FAlloyUtils.py
+++ FAlloyUtils.py
@@ -5,3 +5,5 @@ parser_state = "None"
 fuzzy_relations = []
 
 adjective_mapper = {'none': 'fuzzyFalse', 'slightly': 'fuzzySlightly', 'half' : 'fuzzyHalf', 'mostly': 'fuzzyMostly', 'fully': 'fuzzyTrue'}
+
+phase = 0
